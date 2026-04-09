79056a89df4415746b29ad523f22cbdea29fc802
Manuel Schmid <manuel.schmid@js-tut.de>
2026-04-09 23:52:45 +0200

    chore(core): add Spring Boot and Gradle project skeleton

---
+++ ibanify-core/src/main/java/com/manuweb/ibanifycore/IbanifyCoreApplication.java
@@ -0,0 +1,13 @@
+package com.manuweb.ibanifycore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbanifyCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IbanifyCoreApplication.class, args);
+    }
+
+}
